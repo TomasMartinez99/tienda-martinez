41f51f11c763c97339317dcc0795d906d66671c9
Tomas Martinez <tomas.martinez.visintini@gmail.com>
2021-06-08 20:29:18 -0300

    Desafío: Contador con botón

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
 
-export const ItemCount = ({ stock, initial, onAdd }) => {
+export const ItemCount = ({ stock, initial }) => {
     const [count, setCount] = useState(initial);
 
-    const incrementar = () => {
-        setCount(count + 1);
+    const onAdd = () => {
+        count < stock && setCount(count + 1)
     }
 
     const decrementar = () => {
-        setCount(count - 1);
+        count > 0 && setCount(count - 1)
     }
 
     return <>
         <div className="agregarCarrito">
             <button onClick={e => decrementar()}>-</button>
-            {count > 0 ? <input value={count}/> : <input value="0"/>}
-            <button onClick={e => incrementar()}>+</button>
+            <h2>{count}</h2>
+            <button onClick={e => onAdd()}>+</button>
             <p>Agregar al carrito</p>
         </div>
     </>
