9069ec61db350315913d8e86a3102315ef9eaefd
Tomas Martinez <tomas.martinez.visintini@gmail.com>
2021-06-07 22:51:53 -0300

    Botón contador

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
 
 export const ItemCount = ({ stock, initial, onAdd }) => {
-    const [{initial}, setInitial] = useState({initial});
+    const [count, setCount] = useState(initial);
 
     const incrementar = () => {
-        setInitial(initial + 1);
+        setCount(count + 1);
     }
 
     const decrementar = () => {
-        setInitial(initial + 1);
+        setCount(count - 1);
     }
 
     return <>
         <div className="agregarCarrito">
             <button onClick={e => decrementar()}>-</button>
-            {initial > 0 ? <input value={ `${initial}` }/> : <input value="0"/>}
+            {count > 0 ? <input value={count}/> : <input value="0"/>}
             <button onClick={e => incrementar()}>+</button>
             <p>Agregar al carrito</p>
         </div>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,7 +5,7 @@ import ItemCount from './ItemCount';
 export const ItemListContainer = props => {
     return <div className="main container">
         <h2>{props.greeting}</h2>
-        <ItemCount stock="5" initial="1"/>
+        <ItemCount stock="5" initial="5"/>
     </div>
 }
 
