1985b3eab090de1bec3d88411153ed68a71708ab
Tomas Martinez <tomas.martinez.visintini@gmail.com>
2021-06-11 19:18:15 -0300

    Desafío: Catálogo con MAPS y Promises

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,46 +3,43 @@ import React, {useState, useEffect} from 'react';
 // Promise
 const promiseItem = () => {
     return new Promise ((resolve, reject) => {
-        setTimeout(() =>
+        setTimeout(() => resolve(
             // Si está todo ok se muestra el resolve
-            resolve([
-                {
-                    id: 1,
-                    title: 'Smartwatch Samsung 1',
-                    price: 25000,
-                    pictureUrl: '../public/img/relojes/reloj-samsung-1.png',
-                    alt: 'Smartwatch Samsung 1'
-                },
-                {
-                    id: 2,
-                    title: 'Smartwatch Samsung 2',
-                    price: 25000,
-                    pictureUrl: '../public/img/relojes/reloj-samsung-2.png',
-                    alt: 'Smartwatch Samsung 2'
-                },
-                {
-                    id: 3,
-                    title: 'Smartwatch Apple watch 1',
-                    price: 25000,
-                    pictureUrl: '../public/img/relojes/apple-watch-1.png',
-                    alt: 'Smartwatch Apple watch 1'
-                }
-            ] 
+            [{
+                id: 1,
+                title: 'Smartwatch Samsung 1',
+                price: 25000,
+                pictureUrl: '../public/img/relojes/reloj-samsung-1.png',
+                alt: 'Smartwatch Samsung 1'
+            },
+            {
+                id: 2,
+                title: 'Smartwatch Samsung 2',
+                price: 25000,
+                pictureUrl: '../public/img/relojes/reloj-samsung-2.png',
+                alt: 'Smartwatch Samsung 2'
+            },
+            {
+                id: 3,
+                title: 'Smartwatch Apple watch 1',
+                price: 25000,
+                pictureUrl: '../public/img/relojes/apple-watch-1.png',
+                alt: 'Smartwatch Apple watch 1'
+            }]
         ), 2000)
     })
-}
+};
 
 export const Item = props => {
-    
     // Hook useState
     const [dataToShow, setDataToShow] = useState([]);
 
-    // Función encargada de llamar a mi promesa
+    // Función encargada de llamar a mi Promise
     const ejecutarItem = () => {
         promiseItem().then(relojes)
     }
 
-    // Hook useEffect
+    // Hook useEffect (cada vez que se ejecuta este componente, se ejecuta la función ejecutarItem())
     useEffect(() => {
         ejecutarItem();
     }, [])
