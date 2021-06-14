5bacbf355e23f9f501f4045a3ca442dd510b4f81
Tomas Martinez <tomas.martinez.visintini@gmail.com>
2021-06-14 19:35:30 -0300

    Detalle de Producto

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export const ItemCount = ({ stock, initial }) => {
-    const [count, setCount] = useState(initial);
+  const [count, setCount] = useState(initial);
 
-    const onAdd = () => {
-        count < stock && setCount(count + 1)
-    }
+  const onAdd = () => {
+    count < stock && setCount(count + 1);
+  };
 
-    const decrementar = () => {
-        count > 0 && setCount(count - 1)
-    }
+  const decrementar = () => {
+    count > 0 && setCount(count - 1);
+  };
 
-    return <>
-        <div className="agregarCarrito">
-            <button onClick={e => decrementar()}>-</button>
-            <h2>{count}</h2>
-            <button onClick={e => onAdd()}>+</button>
-            <p>Agregar al carrito</p>
-        </div>
+  return (
+    <>
+      <div className="agregarCarrito">
+        <button className="btn-menos" onClick={(e) => decrementar()}>
+          -
+        </button>
+        <h2>{count}</h2>
+        <button onClick={(e) => onAdd()}>+</button>
+        <p>Agregar al carrito</p>
+      </div>
     </>
-}
+  );
+};
 
 export default ItemCount;
